abe3f8056adf42cdc205b92f34b6347f7afbbfa2
Gabor Szabo <gabor@szabgab.com>
2014-09-11 15:49:00 +0300

    copy Python support from Padre

---
+++ lib/List/Functions/Python.pm
@@ -0,0 +1,41 @@
+package List::Functions::Python;
+
+use 5.008;
+use strict;
+use warnings;
+use List::Functions;
+
+our $VERSION = '1.01';
+our @ISA     = qw(List::Functions);
+
+######################################################################
+
+my $n = "\\cM?\\cJ";
+our $function_search_re = qr/
+		(?:
+			\"\"\".*?\"\"\"
+			|
+			(?:^|$n)\s*
+			(?:
+				(?:def)\s+(\w+)
+				|
+				(?:(\w+)\s*\=\s*lambda)
+			)
+		)
+	/sx;
+
+sub find {
+	my ($self, $text, $sort) = @_;
+
+	my @functions = grep { defined $_ } $text =~ /$function_search_re/g;
+
+	return @{ $self->sort_functions( \@functions, $sort ) };
+}
+
+1;
+
+# Copyright 2008-2013 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
+
