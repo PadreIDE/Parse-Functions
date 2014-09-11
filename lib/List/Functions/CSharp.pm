f758d271f829af17d5f6cd0ba857175cb5cd1b62
Gabor Szabo <gabor@szabgab.com>
2014-09-11 16:09:33 +0300

    CSharp support copied from Padre

---
+++ lib/List/Functions/CSharp.pm
@@ -0,0 +1,56 @@
+package List::Functions::CSharp;
+
+use 5.008;
+use strict;
+use warnings;
+use List::Functions;
+
+our $VERSION = '1.01';
+our @ISA     = qw(List::Functions);
+
+######################################################################
+# Padre::Task::FunctionList Methods
+
+my $newline =
+	qr{\cM?\cJ}; # recognize newline even if encoding is not the platform default (will not work for MacOS classic)
+my $method_search_regex = qr{
+			/\*.+?\*/        # block comment
+			|
+			\/\/.+?$newline  # line comment
+			|
+			(?:^|$newline)   # text start or newline 
+			\s*              
+			(?:
+			  (?: \[ [\s\w()]+ \]\s* )?  # optional annotations
+			  (?:
+				(?: public|protected|private|
+				    abstract|static|sealed|virtual|override|
+				    explicit|implicit|
+				    operator|
+				    extern)
+				\s+
+			  ){0,4}                     # zero to 2 method modifiers
+			  (?: [\w\[\]<>,]+)          # return data type
+			  \s+
+			  (\w+)                      # method name
+			  (?: <\w+>)?                # optional: generic type parameter
+			  \s*
+			  \(.*?\)                    # parentheses around the parameters
+			)
+	}sx;
+
+sub find {
+	my ($self, $text, $sort) = @_;
+
+	my @functions = grep { defined $_ } $text =~ /$method_search_regex/g;
+
+	return @{ $self->sort_functions( \@functions, $sort ) };
+}
+
+1;
+
+# Copyright 2008-2013 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
+
